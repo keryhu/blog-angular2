c2911bde09c1f45548416988db382beadd4ea76a
keryhu <keryhu@hotmail.com>
2017-05-11 15:52:13 +0800

    修补 blogSimpleComponent，详细blog url更新不及时的bug

--- src/app/common/blogs/blog-simple/blog-simple.component.ts
+++ src/app/common/blogs/blog-simple/blog-simple.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {SimpleBlog} from "../simple-blog.model";
 import {blogUrl} from "../../../core/index";
 
@@ -9,11 +9,11 @@ import {blogUrl} from "../../../core/index";
   templateUrl: './blog-simple.component.html',
   styleUrls: ['./blog-simple.component.css']
 })
-export class BlogSimpleComponent implements OnInit {
+export class BlogSimpleComponent implements OnInit,OnChanges {
+
 
   private tags = [];
-  public detailBlogUrl:string;
-
+  public  detailBlogUrl:string;
 
   constructor() {
   }
@@ -22,8 +22,16 @@ export class BlogSimpleComponent implements OnInit {
   @Input()
   public simpleblog: SimpleBlog;
 
+
   ngOnInit() {
     this.tags = this.simpleblog.tags;
+    this.detailBlogUrl=`${blogUrl}/${this.simpleblog.id}`;
+  }
+
+  // 必须要使用这个  implements OnChanges，因为 BlogSimpleComponent是一个child component
+  //  每次前台BlogHomeComponent 更新 blog数据的时候，
+  // this.detailBlogUrl 是通过计算 后，获取的数据，仅仅通过ngOnInit 无法刷新 detailBlogUrl的值。
+  ngOnChanges(changes: SimpleChanges): void {
     this.detailBlogUrl=`${blogUrl}/${this.simpleblog.id}`;
   }
 
